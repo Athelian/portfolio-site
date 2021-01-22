a668dd3896ba1ee348f4d8c37514c0a79879cf93
Eliot Austin-Forbes <ejaustinforbes@gmail.com>
2021-01-22 10:14:14 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='menu'>
+      <div className="title-box" id="title-box">
+        <span id="a">Eliot <br/>Austin<br/> Forbes</span>
+        <button className='link' id='about'><span>About</span></button>
+        <button className='link' id='projects'><span>Projects</span></button>
+        <button className='link' id='github'><span>GitHub</span></button>
+      </div>
     </div>
   );
 }
